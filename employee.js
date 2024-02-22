cd16a01a275709e83fc7a69c9d76f1fc3d729bda
Liam Dinh <loiphatdinh@gmail.com>
2024-02-22 08:42:22 -0600

    finished phase 3 - Dynamically adding employees

--- employee.js
+++ employee.js
@@ -4,11 +4,15 @@ class Employee {
         this.salary = salary;
         this.title = title;
         this.manager = manager;
+
+        if (this.manager) {
+            this.manager.addEmployee(this);
+        }
     }
 }
 
 module.exports = Employee;
 
 // testing Employee class
-const leo = new Employee('leonardo', 90000, 'Ninja');
-console.log(leo);
+/* const leo = new Employee('leonardo', 90000, 'Ninja');
+console.log(leo); */
